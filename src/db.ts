2db93fd731c526380fa95c7d6e1d0655a3fee271
alexander flores <alexfloresrayme@gmail.com>
2026-05-12 20:41:27 -0500

    C

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from "fs";
-import * as path from "path";
+import path from "node:path";
+import fs from "node:fs/promises";
 import { getLogger } from "./logger";
 
 const logger = getLogger("db");
